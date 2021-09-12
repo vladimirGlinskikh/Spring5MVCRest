63dc3fc42fe12c0646e7fe177a87d8bac404006e
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2021-09-12 21:56:43 +0600

    MapStruct IntelliJ Plugin

--- src/main/java/kz/zhelezyaka/api/v1/mapper/CategoryMapper.java
+++ src/main/java/kz/zhelezyaka/api/v1/mapper/CategoryMapper.java
@@ -3,10 +3,13 @@ package kz.zhelezyaka.api.v1.mapper;
 import kz.zhelezyaka.api.v1.model.CategoryDTO;
 import kz.zhelezyaka.domain.Category;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
 public interface CategoryMapper {
     CategoryMapper INSTANCE = Mappers.getMapper(CategoryMapper.class);
+
+    @Mapping(source = "id", target = "id")
     CategoryDTO categoryToCategoryDTO(Category category);
 }
