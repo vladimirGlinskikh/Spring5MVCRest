0519b606d40ff6d6f4f07a985ead870cc7a1f06b
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2021-09-14 22:48:54 +0600

    Category Service

---
+++ src/main/java/kz/zhelezyaka/services/CategoryService.java
@@ -0,0 +1,11 @@
+package kz.zhelezyaka.services;
+
+import kz.zhelezyaka.api.v1.model.CategoryDTO;
+
+import java.util.List;
+
+public interface CategoryService {
+    List<CategoryDTO> getAllCategories();
+
+    CategoryDTO getCategoryByName(String name);
+}
