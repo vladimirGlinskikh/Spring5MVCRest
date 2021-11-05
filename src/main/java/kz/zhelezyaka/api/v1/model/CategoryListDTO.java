10a607bc7c026b0dded8651ca5816c603a20d30b
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2021-11-05 21:58:25 +0600

    List Categories

---
+++ src/main/java/kz/zhelezyaka/api/v1/model/CategoryListDTO.java
@@ -0,0 +1,12 @@
+package kz.zhelezyaka.api.v1.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+public class CategoryListDTO {
+    List<CategoryDTO> categories;
+}
