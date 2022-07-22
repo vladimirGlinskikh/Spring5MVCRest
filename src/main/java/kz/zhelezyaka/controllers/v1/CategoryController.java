9768378395e1126a25b49d876b8dcff9e114e823
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2022-07-22 10:55:58 +0300

    Spring Rest Controller

--- src/main/java/kz/zhelezyaka/controllers/v1/CategoryController.java
+++ src/main/java/kz/zhelezyaka/controllers/v1/CategoryController.java
@@ -4,13 +4,9 @@ import kz.zhelezyaka.api.v1.model.CategoryDTO;
 import kz.zhelezyaka.api.v1.model.CategoryListDTO;
 import kz.zhelezyaka.services.CategoryService;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 @RequestMapping(CategoryController.BASE_URL)
 public class CategoryController {
     public static final String BASE_URL = "/api/v1/categories/";
@@ -21,14 +17,14 @@ public class CategoryController {
     }
 
     @GetMapping
-    public ResponseEntity<CategoryListDTO> getAllCategories() {
-        return new ResponseEntity<CategoryListDTO>(
-                new CategoryListDTO(categoryService.getAllCategories()), HttpStatus.OK);
+    @ResponseStatus(HttpStatus.OK)
+    public CategoryListDTO getAllCategories() {
+        return new CategoryListDTO(categoryService.getAllCategories());
     }
 
     @GetMapping("{name}")
-    public ResponseEntity<CategoryDTO> getCategoryByName(@PathVariable String name) {
-        return new ResponseEntity<CategoryDTO>(
-                categoryService.getCategoryByName(name), HttpStatus.OK);
+    @ResponseStatus(HttpStatus.OK)
+    public CategoryDTO getCategoryByName(@PathVariable String name) {
+        return categoryService.getCategoryByName(name);
     }
 }
