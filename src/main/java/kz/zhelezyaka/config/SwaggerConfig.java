54bf82ea37e467ee892c669a1802cc3cde95d603
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2022-07-25 19:59:15 +0300

    Swagger UI

--- src/main/java/kz/zhelezyaka/config/SwaggerConfig.java
+++ src/main/java/kz/zhelezyaka/config/SwaggerConfig.java
@@ -2,6 +2,8 @@ package kz.zhelezyaka.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -10,7 +12,8 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
 @Configuration
-public class SwaggerConfig {
+public class SwaggerConfig /*extends WebMvcConfigurationSupport*/ {
+
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
@@ -20,4 +23,13 @@ public class SwaggerConfig {
                 .build()
                 .pathMapping("/");
     }
+
+//    @Override
+//    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler("swagger-ui.html")
+//                .addResourceLocations("classpath:/META-INF/resources/");
+//
+//        registry.addResourceHandler("/webjars/**")
+//                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+//    }
 }
